7d36f7ec88148ef662c21a96796f41f7807bb112
Fhadshnde <Fhadshnde32@gmail.com>
2024-12-29 04:33:58 +0300

    Adding frontend files

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
+app.listen("5000" || process.env.PORT, () => {
   console.log("Backend is running.");
 });
